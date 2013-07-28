a7bf9664b34b61b30ac70c0978635f71a47ee9c5
Max <deepthought@gmail.com>
2013-07-29 00:08:54 +0800

    fix includes

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.on_use(function(api) {
   api.use("coffeescript", ['client', 'server']);
   api.use(['templating'], 'client');
 
-  api.add_files(['client/style.css', 'client/db_view.html', 'client/collection_view.html', 'client/document_view.html'], 'client');
+  api.add_files(['client/db_view.html', 'client/collection_view.html', 'client/document_view.html'], 'client');
   api.add_files(['client/router.coffee', 'client/collection_view.coffee', 'client/document_view.coffee'], 'client');
   
   api.add_files('lib/collections.coffee', ['client', 'server']);
